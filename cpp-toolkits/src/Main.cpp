9eb175c53a8c9e24b8d3da4523ecf9df0fef5282
Chun-Yi Kuo <kingrobber@gmail.com>
2020-09-11 01:41:13 +0800

    add draft for timer

--- cpp-toolkits/src/Main.cpp
+++ cpp-toolkits/src/Main.cpp
@@ -2,9 +2,17 @@
 
 #include <iostream>
 
+// Demo for Timer
+void PrintFunction(int N)
+{	
+	cytools::Timer timer;
+	for (int i = 0; i < N; i++)
+		std::cout << "Hello!\n";// << std::endl;
+}
+
 int main()
 {
-	cytools::PrintMessage();
-
+	// Timer Demo
+	PrintFunction(100);
 	std::cin.get();
 }
--- toolkits/src/Timing.cpp
+++ toolkits/src/Timing.cpp
@@ -1,10 +1,7 @@
 #include "Timing.h"
 
-#include <iostream>
+
 
 namespace cytools {
-	void PrintMessage()
-	{
-		std::cout << "Hello There!" << std::endl;
-	}
+
 }
