6af326267fb8a254712db7a877c567ef1d7079e6
Chun-Yi Kuo <kingrobber@gmail.com>
2020-09-13 21:44:51 +0800

    [feat] add precompile header

---
+++ toolkits/src/pch.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <iostream>
+#include <iomanip>
+#include <chrono>
+#include <algorithm>
+
+// Data structure
+#include <string>
+
+// File Handling
+#include <fstream>
+
+// Multithreading
+#include <thread>
